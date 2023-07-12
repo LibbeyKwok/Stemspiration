de28c6f86b32d0548dce0bb326e4afdf34574869
LibbeyKwok <libbeykwok8@gmail.com>
2023-07-12 15:02:10 -0700

    Initial Commit

---
+++ Stemspiration/StemspirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StemspirationApp.swift
+//  Stemspiration
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StemspirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
